098abada67b9965f7f63b2ccbb9b3c2841733a54
james <90643139+Jamcha123@users.noreply.github.com>
2023-03-13 18:19:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-var index = 0; 
-showList();
-function press(n){
-    showList(index += n);
-}
-function showList(n){
-    let x = document.querySelectorAll('.open');
-    for(let i = 0; i < x.length; i++){
-        x[i].style.display = "none";
-    }
-    if(n === 1){
-        for(let i = 0; i < x.length; i++){
-            x[i].style.display = "block";
-        }
-    }else if(n == 2){
+var x = document.getElementsByClassName("text");
+class thing
+{
+    default(){
         for(let i = 0; i < x.length; i++){
             x[i].style.display = "none";
         }
-    }else{
-        console.error("error");
+    }
+    open(){
+        for(let i = 0; i < x.length; i++){
+            x[i].style.display = "block";
+        }
+    }
+    close(){
+        for(let i = 0; i < x.length; i++){
+            x[i].style.display = "none";
+        }
     }
 }
+let mything = new thing();
+let myPromise  = new Promise(function(resolve, reject){
+    resolve();
+    reject();
+})
+myPromise.then(
+    function(value){
+        mything.default();
+        document.getElementById("enter").addEventListener(('click'), (e) => {
+            mything.open();
+        })
+        document.getElementById("exit").addEventListener(('click'), (e) => {
+            mything.close();
+        })
+    },
+    function(error){
+        console.error("error");
+    }
+)
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
@@ -7,8 +7,25 @@ const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerH
 camera.position.setZ(30);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg1'),
+  canvas: document.querySelector('#bg1'),
 });
-renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.render(scene, camera);
+
+function addstuff(target){
+  if(target === 0){
+    return 0;
+  }else{
+    const spheregeometry = new THREE.SphereGeometry(20, 10);
+    const spherematerial = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe: true});
+  
+    const sphere = new THREE.Mesh(spheregeometry, spherematerial);
+    sphere.position.set(Math.random() * window.innerWidth - window.innerHeight, Math.random() * window.innerWidth - window.innerHeight, Math.random() * 1800 - 900);
+
+    scene.add(sphere);
+    renderer.render(scene, camera);
+    addstuff(target-1);
+  }
+}
+addstuff(750);
